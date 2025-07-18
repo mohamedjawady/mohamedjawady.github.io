bb8ed066cd5f64016ff59c015ffaf0a10dba6db8
mohamedjawady <mohamedhabib.jaouadi@etudiant-isi.utm.tn>
2025-07-18 12:43:15 +0100

    added better browser support

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,10 +25,15 @@ export const metadata: Metadata = {
   creator: "Mohamed Habib Jaouadi",
   metadataBase: new URL(baseUrl),
   icons: {
-    icon: "/favicon.ico",
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon.ico", sizes: "any" }
+    ],
     shortcut: "/favicon.ico",
-    apple: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
   },
+  manifest: "/site.webmanifest",
   openGraph: {
     type: "website",
     locale: "en_US",
